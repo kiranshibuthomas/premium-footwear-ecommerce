3c5f228b80363eb1af6c9705989bd62c1e433f89
Kiran SHibu Thomas <kiranshibuthomas17603@gmail.com>
2025-05-19 13:25:03 +0530

    updated Database

--- ecommerce_db.sql
+++ ecommerce_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 18, 2025 at 03:34 PM
+-- Generation Time: May 19, 2025 at 09:54 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -57,6 +57,14 @@ CREATE TABLE `cart_items` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `cart_items`
+--
+
+INSERT INTO `cart_items` (`id`, `user_id`, `product_id`, `quantity`, `created_at`, `updated_at`) VALUES
+(8, 1, 6, 1, '2025-05-19 03:04:33', '2025-05-19 03:04:33'),
+(9, 1, 7, 1, '2025-05-19 03:04:34', '2025-05-19 03:04:34');
+
 -- --------------------------------------------------------
 
 --
@@ -73,6 +81,14 @@ CREATE TABLE `orders` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `total_amount`, `status`, `shipping_address`, `created_at`, `updated_at`) VALUES
+(1, 1, 160.98, 'pending', 'testaddress@okplace', '2025-05-18 14:54:42', '2025-05-18 14:54:42'),
+(2, 3, 12.99, 'pending', 'awdsawdawdawdaw', '2025-05-18 16:00:23', '2025-05-18 16:00:23');
+
 -- --------------------------------------------------------
 
 --
@@ -86,6 +102,16 @@ CREATE TABLE `order_items` (
   `quantity` int(11) NOT NULL,
   `price_at_time` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price_at_time`) VALUES
+(1, 1, 9, 1, 30.99),
+(2, 1, 8, 1, 29.99),
+(3, 1, 7, 1, 100.00),
+(4, 2, 10, 1, 12.99);
 
 -- --------------------------------------------------------
 
@@ -109,9 +135,17 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `stock`, `image_path`, `category`, `created_at`) VALUES
-(1, 'Smartphone X', 'Latest smartphone with amazing features', 999.99, 50, NULL, 'Electronics', '2025-05-18 12:48:34'),
-(2, 'Laptop Pro', 'High-performance laptop for professionals', 1499.99, 30, NULL, 'Electronics', '2025-05-18 12:48:34'),
-(3, 'Wireless Earbuds', 'Premium wireless earbuds with noise cancellation', 199.99, 100, NULL, 'Electronics', '2025-05-18 12:48:34');
+(6, 'Shoe 2', 'NEW', 99.99, 10, 'product_1747578547_f694a45f5f3829b8.jpg', 'Sports', '2025-05-18 14:29:07'),
+(7, 'Shoe3', 'NEW', 100.00, 9, 'product_1747582786_f55924464c4a25ef.jpg', 'Sports', '2025-05-18 14:29:31'),
+(8, 'Shoe 4', 'NEW', 29.99, 5, 'product_1747578592_c616204dfda1287a.jpg', 'Sports', '2025-05-18 14:29:52'),
+(9, 'Shoe 4', 'NEW', 30.99, 9, 'product_1747578611_44d6124a58be0740.jpg', 'Sports', '2025-05-18 14:30:11'),
+(10, 'Shoe 4', 'NEW', 12.99, 11, 'product_1747583223_cd5b02a7af3d7ef8.jpg', 'Sports', '2025-05-18 15:47:03'),
+(11, 'Shoe 4', 'NEW', 166.99, 10, 'product_1747583245_8aea47699d8f619b.jpg', 'Sports', '2025-05-18 15:47:25'),
+(12, 'Shoe 4', 'NEW', 79.99, 19, 'product_1747583260_5768e0a0202682c4.jpg', 'Sports', '2025-05-18 15:47:40'),
+(13, 'Shoe 4', 'NEW', 66.99, 12, 'product_1747583277_6688a7c91cb33fef.jpg', 'Sports', '2025-05-18 15:47:57'),
+(14, 'Shoe 4', 'NEW', 49.99, 12, 'product_1747583307_e064ea82b5106520.jpg', 'Sports', '2025-05-18 15:48:27'),
+(15, 'Shoe 4', 'NEW', 70.99, 12, 'product_1747583325_9f64f49bfa5787a0.jpg', 'Sports', '2025-05-18 15:48:45'),
+(16, 'Shoe 4', 'NEW', 39.99, 30, 'product_1747583347_bbeb68bd32d89e3b.jpg', 'Sports', '2025-05-18 15:49:07');
 
 -- --------------------------------------------------------
 
@@ -119,17 +153,22 @@ INSERT INTO `products` (`id`, `name`, `description`, `price`, `stock`, `image_pa
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-    id int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(100) NOT NULL,
-    email varchar(100) NOT NULL,
-    password varchar(255) NOT NULL,
-    created_at timestamp NOT NULL DEFAULT current_timestamp(),
-    PRIMARY KEY (id),
-    UNIQUE KEY email (email)
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
+(1, 'test', 'test@gmail.com', '$2y$10$CGOGVIwuxSukzoUcOErx3eLQT7oxNQsya1KPAiLxfBOs5gj0Q.puq', '2025-05-18 14:07:51'),
+(2, 'example', 'example@gmail.com', '$2y$10$6M5TrPd6WLmPARO30siHS.a5CiCnulrND9TM5/LS6KEf8W3zYsUju', '2025-05-18 14:42:17'),
+(3, 'example2', 'example2@gmail.com', '$2y$10$uqnBcmISLRAgqcCBY/QQ5eQuniKi9.uU8v9XYUhAiOx7j6jwoR5ra', '2025-05-18 15:59:57');
 
 --
 -- Indexes for dumped tables
@@ -155,7 +194,7 @@ ALTER TABLE `cart_items`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
+  ADD KEY `orders_ibfk_1` (`user_id`);
 
 --
 -- Indexes for table `order_items`
@@ -172,6 +211,13 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -179,37 +225,37 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `cart_items`
 --
 ALTER TABLE `cart_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
